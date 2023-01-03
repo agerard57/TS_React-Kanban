8e00b0ef11a8c5a57ad6e505f07780c02582c74f
a.gerard <a.gerard@sesamm.com>
2023-01-03 21:59:08 +0100

    feat: Add routeManager

---
+++ client/src/routeManager/components/ProtectedRoute.tsx
@@ -0,0 +1,11 @@
+import { FC, ReactNode } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+type Props = {
+  isAllowed: boolean;
+  redirectPath?: string;
+  children: ReactNode;
+};
+
+export const ProtectedRoute: FC<Props> = ({ isAllowed, redirectPath = '/home', children }) =>
+  isAllowed ? children ? <>{children}</> : <Outlet /> : <Navigate to={redirectPath} replace />;
---
+++ client/src/routeManager/components/RouteManager.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+import MediaQuery from 'react-responsive';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+import { Mobile, Layout } from '../../core';
+
+/* import { LoadingScreen } from '../../loadingScreen'; */
+/* import { ProtectedRoute } from './ProtectedRoute'; */
+
+const RouteManager: FC = () => {
+  /*   const { isUserLoggedIn, loggedUserData, loading } = useRouteManager(); */
+  /* if (loading) return <LoadingScreen />; */
+
+  return (
+    <Router>
+      <MediaQuery maxDeviceWidth={1224}>
+        <Mobile />
+      </MediaQuery>
+      <MediaQuery minDeviceWidth={1224}>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+
+            <Route path="/home" element={<h1>Deez</h1>} />
+            {/*               <Route
+                path="/user/"
+                element={
+                  <ProtectedRoute isAllowed={isUserLoggedIn}>
+                    <ProfilePage loggedUserData={loggedUserData} />
+                  </ProtectedRoute>
+                }
+              /> */}
+          </Routes>
+        </Layout>
+      </MediaQuery>
+    </Router>
+  );
+};
+
+export default RouteManager;
