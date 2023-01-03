8ca3e3d741b9a5eacd08d59988aee62e295d92d4
GERARD Alexandre <agerard57@protonmail.com>
2023-01-03 23:43:50 +0100

    feat: Fix typo

--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -3,53 +3,58 @@ const genericCRUDController = require('./genericCRUDController');
 const userServices = require('../services/userService');
 
 exports.register = async (req, res) => {
-    const userExists = await userServices.userExists(req.body.email);
-    if (userExists) {
-      res.status(409).json({
-        message: 'User already exists!',
-      });
-      return;
-    } 
-
-    const user = new UserModel({
-      ...req.body,
+  const userExists = await userServices.userExists(req.body.email);
+  if (userExists) {
+    res.status(409).json({
+      message: 'User already exists!',
     });
-    user
-      .save()
-      .then(() => {
-          userServices.loginUser(req.body.email, req.body.password).then((response) => {
-            res.status(200).json({
-                token : response.token,
-                message: 'User registered!',
-                data : user
-            });
-          }
-        ).catch((error) => {
-            res.status(500).json({
-                message: "an error occured",
-                error
-            });
+    return;
+  }
+
+  const user = new UserModel({
+    ...req.body,
+  });
+  user
+    .save()
+    .then(() => {
+      userServices
+        .loginUser(req.body.email, req.body.password)
+        .then((response) => {
+          res.status(200).json({
+            token: response.token,
+            message: 'User registered!',
+            data: user,
+          });
+        })
+        .catch((error) => {
+          res.status(500).json({
+            message: 'an error occurred',
+            error,
+          });
         });
-      })
-      .catch((error) => {
-        res.status(500).json({
-          message: error,
-        });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: error,
       });
+    });
 };
 
 exports.login = (req, res) => {
-    userServices.loginUser(req.body.email, req.body.password).then((response) => {
-        res.status(200).json({
-            token : response.token,
-            data : response.user,
-            message: 'User logged in!',
-        });
-    }).catch((error) => {
-        res.status(500).json({
-            message: "an error occured",
-            error
-        });
+  userServices
+    .loginUser(req.body.email, req.body.password)
+    .then((response) => {
+      res.status(200).json({
+        token: response.token,
+        data: response.user,
+        message: 'User logged in!',
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: 'an error occured',
+        error,
+      });
     });
 };
 
