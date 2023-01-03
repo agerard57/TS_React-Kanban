a9b2faabb3566da2eec289a4a628265af8ea561e
Vincent REMY <vincentremy222@gmail.com>
2023-01-03 22:55:41 +0100

    feat: getOne patch and delete on user endpoint implemented

--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -5,5 +5,9 @@ module.exports = (app) => {
   app.route("/user/login").post(userController.login);
   app.route("/user/register").post(userController.register);
   app.route("/users").get(userController.getAll);
+  app.route("/user/:id([0-9]*)")
+    .get(userController.getOne)
+    .delete(userController.delete)
+    .patch(userController.patch);
 
 };
