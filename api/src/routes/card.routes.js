ae1d4909faffe7a031e25a5a3b3aff4bd42b1fec
a.gerard <a.gerard@sesamm.com>
2023-01-04 09:46:04 +0100

    feat: Format fix

--- api/src/routes/card.routes.js
+++ api/src/routes/card.routes.js
@@ -2,12 +2,17 @@ const cardController = require('../controllers/card.controller');
 const authMiddleware = require('../middleware/auth.middleware');
 
 module.exports = (app) => {
-  app.route('/cards').get([authMiddleware.checkTokenMiddleware],cardController.getAll);
+  app
+    .route('/cards')
+    .get([authMiddleware.checkTokenMiddleware], cardController.getAll);
 
-  app.route('/card/:id([0-9]*)')
-    .get([authMiddleware.checkTokenMiddleware],cardController.getOne)
-    .delete([authMiddleware.checkTokenMiddleware],cardController.delete)
-    .patch([authMiddleware.checkTokenMiddleware],cardController.patch);
+  app
+    .route('/card/:id([0-9]*)')
+    .get([authMiddleware.checkTokenMiddleware], cardController.getOne)
+    .delete([authMiddleware.checkTokenMiddleware], cardController.delete)
+    .patch([authMiddleware.checkTokenMiddleware], cardController.patch);
 
-  app.route('/card').post([authMiddleware.checkTokenMiddleware],cardController.add);
+  app
+    .route('/card')
+    .post([authMiddleware.checkTokenMiddleware], cardController.add);
 };
