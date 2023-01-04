7b9db69a211c2c73e80254c387b93a9b82d2cd65
GERARD Alexandre <agerard57@protonmail.com>
2023-01-04 02:35:06 +0100

    feat: Improve language support

--- client/src/core/index.ts
+++ client/src/core/index.ts
@@ -1,11 +1,11 @@
 export * as i18n from './i18n';
 
-export { Layout, ColoredBackground, Mobile, LandscapeMode } from './components';
+export { Layout, ColoredBackground, Mobile } from './components';
 
 export { cookiesManager, normalizeDate, normalizeTime, logout } from './helpers';
 
 export type { AuthToken, LoggedUserDataProps } from './types';
 
-export { usePageTitle } from './hooks';
+export { usePageTitle, useCustomToast } from './hooks';
 
 export { tokenService, privateAxios, publicAxios } from './services';
