40191852ca7f5d480a53476fc5fa9d2034ed5440
a.gerard <a.gerard@sesamm.com>
2023-01-04 09:46:04 +0100

    feat: Format fix

--- api/src/middleware/auth.middleware.js
+++ api/src/middleware/auth.middleware.js
@@ -1,28 +1,27 @@
 const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth.config');
 
-const extractBearerToken = headerValue => {
-    if (typeof headerValue !== 'string') {
-        return false;
-    }
+const extractBearerToken = (headerValue) => {
+  if (typeof headerValue !== 'string') {
+    return false;
+  }
 
-    const matches = headerValue.match(/(bearer)\s+(\S+)/i);
-    return matches && matches[2];
-}
+  const matches = headerValue.match(/(bearer)\s+(\S+)/i);
+  return matches && matches[2];
+};
 
 exports.checkTokenMiddleware = (req, res, next) => {
-    
-    const token = req.headers.authorization && extractBearerToken(req.headers.authorization);
+  const token =
+    req.headers.authorization && extractBearerToken(req.headers.authorization);
 
-    if (!token) {
-        return res.status(401).json({ message: 'Error. No Baerer token provided' });
+  if (!token) {
+    return res.status(401).json({ message: 'Error. No Baerer token provided' });
+  }
+
+  return jwt.verify(token, authConfig.secret, (err) => {
+    if (err) {
+      return res.status(401).json({ message: 'Error. The token is not valid' });
     }
-
-    jwt.verify(token, authConfig.secret , (err, decodedToken) => {
-        if (err) {
-            res.status(401).json({ message: 'Error. The token is not valid' });
-        } else {
-            return next();
-        }
-    })
-}
+    return next();
+  });
+};
--- api/src/routes/list.routes.js
+++ api/src/routes/list.routes.js
@@ -2,14 +2,17 @@ const listController = require('../controllers/list.controller');
 const authMiddleware = require('../middleware/auth.middleware');
 
 module.exports = (app) => {
-  app.route('/lists').get([authMiddleware.checkTokenMiddleware],listController.getAll);
+  app
+    .route('/lists')
+    .get([authMiddleware.checkTokenMiddleware], listController.getAll);
 
-  app.route("/list/:id([0-9]*)")
-    .get([authMiddleware.checkTokenMiddleware],listController.getOne)
-    .delete([authMiddleware.checkTokenMiddleware],listController.delete)
-    .patch([authMiddleware.checkTokenMiddleware],listController.patch);
-    
-  app.route('/list')
-    .post([authMiddleware.checkTokenMiddleware],listController.add)
+  app
+    .route('/list/:id([0-9]*)')
+    .get([authMiddleware.checkTokenMiddleware], listController.getOne)
+    .delete([authMiddleware.checkTokenMiddleware], listController.delete)
+    .patch([authMiddleware.checkTokenMiddleware], listController.patch);
 
+  app
+    .route('/list')
+    .post([authMiddleware.checkTokenMiddleware], listController.add);
 };
