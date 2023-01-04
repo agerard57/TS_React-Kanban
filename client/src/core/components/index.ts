733d871f8e266f4905740ecfab5dad920b6a94a1
a.gerard <a.gerard@sesamm.com>
2023-01-04 14:24:52 +0100

    feat: Add color vignette

--- client/src/core/components/index.ts
+++ client/src/core/components/index.ts
@@ -1,3 +1,5 @@
 export { Layout } from './Layout';
 export { ColoredBackground } from './ColoredBackground';
 export { Mobile } from './Mobile';
+export { KanbanList } from './kanban';
+export { TextInput } from './TextInput';
