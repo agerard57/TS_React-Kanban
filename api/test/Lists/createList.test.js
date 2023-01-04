4b5b949e33c870a2e918ba270ecdf2a5a339f84d
Mordreckk <mathoulolivier@gmail.com>
2023-01-04 15:39:54 +0100

    feat: :sparkles: add test

--- api/test/Lists/createList.test.js
+++ api/test/Lists/createList.test.js
@@ -10,38 +10,36 @@ module.exports = () => {
     const app = express();
     const request = require('supertest');
 
-
-    console.log("Test create List with title Tset if execute equal 200");
     app.post(routeList, function(req, res) {
       res.status(200).json({
         title: "Tset",
         user_id: 5
       });
     });
-  
+    
     request(app)
-      .post(routeList)
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err, res) {
-        if (err) {
-          throw err;
-        } else {
-          console.log(res.body);
-        }
+    .post(routeList)
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function(err, res) {
+      console.log("\n\nTest create List with title Tset, should return 200 http code " + JSON.stringify(res.body));
+      if (err) {
+        throw err;
+      } else {
+        console.log("Test pass, result status : " + res.status);
+      }
     });
   }
 
-  function createList_isnotrgtdnjfsvrdtng() {
+  function createList_with_no_title_return_500() {
     const express = require('express');
     const app = express();
     const request = require('supertest');
 
-
     //Test create List without title if execute equal 500 
     app.post(routeList, function(req, res) {
       res.status(500).json({
-        // user_id : 5
+        user_id : 5
       });
     });
     
@@ -50,39 +48,43 @@ module.exports = () => {
       .expect('Content-Type', /json/)
       .expect(500)
       .end(function(err, res) {
-        console.log(res.body);
-        if (err) throw err;
-        // console.log(res.body)
+        console.log("\n\nCreate list with no title, should return 500 http code " + JSON.stringify(res.body));
+        if (err) {
+          throw err;
+        } else {
+          console.log("test pass, result status : " + res.status);
+        }
+    });
+  }
+  
+  function createList_with_no_userID_return_500() {
+    const express = require('express');
+    const app = express();
+    const request = require('supertest');
+
+    //Test create List without title if execute equal 500 
+    app.post(routeList, function(req, res) {
+      res.status(500).json({
+        title : "title"
+      });
+    });
+    
+    request(app)
+      .post(routeList)
+      .expect('Content-Type', /json/)
+      .expect(500)
+      .end(function(err, res) {
+        console.log("\n\nCreate list with no user_id, should return 500 http code" + JSON.stringify(res.body));
+        if (err) {
+          throw err;
+        } else {
+          console.log("test pass, result status : " + res.status);
+        }
     });
   }
 
   createList_is_good();
-  createList_isnotrgtdnjfsvrdtng();
+  createList_with_no_title_return_500();
+  createList_with_no_userID_return_500();
 }
-
-// test('Test create List without title if execute equal 500 ', () => {
-//   const result = fetch(routeList,{
-//     method:"post",
-//     body:{
-//       "user_id" : 5
-//     },
-
-//   });
-//   const resJson=result.json();
-//   expect(resJson.status).toBe(500);
-// });
-
-// test('Test create List without user Id if execute equal 500 ', () => {
-//   const result = fetch(routeList,{
-//     method:"post",
-//     body:{
-//       "title" : "Tset"
-//     },
-
-//   });
-//   const resJson = result.json();
-//   expect(resJson.status).toBe(500);
-// });
-// module.exports = function routes(app) {
-// }
 
