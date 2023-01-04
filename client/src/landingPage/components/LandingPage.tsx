a1874fcf345a0b69685433dd452aa398d708c622
a.gerard <a.gerard@sesamm.com>
2023-01-04 14:25:57 +0100

    feat: Add landing page components

--- client/src/landingPage/components/LandingPage.tsx
+++ client/src/landingPage/components/LandingPage.tsx
@@ -1,40 +1,101 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { FC } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 
+import { KanbanList, List } from '../../core';
+import { ColorVignette } from '../../core/components/ColorVignette';
+import { PasswordlessUser } from '../../core/types';
 import { useLandingPage } from '../hooks';
+import { AddList } from './AddList';
+import { UserList } from './UserList';
 
 export const LandingPage: FC = () => {
   const { t } = useTranslation('LandingPage');
 
   useLandingPage();
 
-  const lists = [
+  const author1: PasswordlessUser = {
+    id: 1,
+    name: {
+      firstName: 'Alexandre',
+      lastName: 'GERARD'
+    },
+    email: 'mns@agerard.dev',
+    color: '#000000'
+  };
+
+  const author2: PasswordlessUser = {
+    id: 2,
+    name: {
+      firstName: 'Nicolas',
+      lastName: 'POIRE'
+    },
+    email: 'mns@npoire.dev',
+    color: '#9B5252'
+  };
+
+  const lists: List[] = [
     {
+      id: 1,
       title: 'BoardTitle 1',
-      items: [
+      author: author1,
+      cards: [
         {
-          title: 'item1',
-          description: 'item1Description'
+          id: 1,
+          title: 'CardTitle 1',
+          description: 'CardDescription 1',
+          author: author2,
+          listId: 1
         },
         {
-          title: 'item2',
-          description: 'item2Description'
+          id: 2,
+          title: 'CardTitle 2',
+          description: 'CardDescription 2',
+          author: author1,
+          listId: 1
         }
       ]
     },
     {
+      id: 2,
       title: 'BoardTitle 2',
-      items: [
+      author: author1,
+      cards: [
         {
-          title: 'item3',
-          description: 'item3Description'
+          id: 3,
+          title: 'CardTitle 3',
+          description: 'CardDescription 3',
+          author: author2,
+          listId: 2
         },
         {
-          title: 'item4',
-          description: 'item4Description'
+          id: 4,
+          title: 'CardTitle 4',
+          description: 'CardDescription 4',
+          author: author1,
+          listId: 1
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: 'BoardTitle 3',
+      author: author1,
+      cards: [
+        {
+          id: 5,
+          title: 'CardTitle 5',
+          description: 'CardDescription 5',
+          author: author2,
+          listId: 3
+        },
+        {
+          id: 6,
+          title: 'CardTitle 6',
+          description: 'CardDescription 6',
+          author: author2,
+          listId: 3
         }
       ]
     }
@@ -42,42 +103,28 @@ export const LandingPage: FC = () => {
 
   return (
     <>
-      <Container
+      <h1
         css={css`
-          padding: 5vw 1vw;
+          padding: 10px;
+          margin: 0;
         `}
       >
-        <h1>{t('title')}</h1>
-        <Container
-          css={css`
-            display: flex;
-            flex-direction: row;
-            justify-content: space-between;
-          `}
-        >
-          {/* Lists are row. in them, title and their items */}
+        {t('title')}
+      </h1>
+      <UserList lists={lists} />
+      <div
+        css={css`
+          display: flex;
+        `}
+        className="container-fluid py-2"
+      >
+        <div className="d-flex flex-row flex-nowrap">
           {lists.map((list) => (
-            <Col
-              css={css`
-                padding: 1vw;
-              `}
-            >
-              <h2>{t(list.title)}</h2>
-              <hr />
-              {list.items.map((item) => (
-                <Row
-                  css={css`
-                    padding: 1vw;
-                  `}
-                >
-                  <h3>{t(item.title)}</h3>
-                  <p>{t(item.description)}</p>
-                </Row>
-              ))}
-            </Col>
+            <KanbanList list={list} key={list.id} />
           ))}
-        </Container>
-      </Container>
+          <AddList />
+        </div>
+      </div>
     </>
   );
 };
