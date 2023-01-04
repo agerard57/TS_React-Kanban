c88b72b7c6734c9b437d7dee4fc8c473a5748f71
Vincent REMY <vincentremy222@gmail.com>
2023-01-04 09:21:06 +0100

    edit : need token to access on card, list and part of user endpoint

--- api/src/routes/card.routes.js
+++ api/src/routes/card.routes.js
@@ -1,13 +1,13 @@
 const cardController = require('../controllers/card.controller');
+const authMiddleware = require('../middleware/auth.middleware');
 
 module.exports = (app) => {
-  app.route('/cards').get(cardController.getAll);
+  app.route('/cards').get([authMiddleware.checkTokenMiddleware],cardController.getAll);
 
   app.route('/card/:id([0-9]*)')
-    .get(cardController.getOne)
-    .delete(cardController.delete)
-    .patch(cardController.patch);
+    .get([authMiddleware.checkTokenMiddleware],cardController.getOne)
+    .delete([authMiddleware.checkTokenMiddleware],cardController.delete)
+    .patch([authMiddleware.checkTokenMiddleware],cardController.patch);
 
-  app.route('/card').post(cardController.add);
-
+  app.route('/card').post([authMiddleware.checkTokenMiddleware],cardController.add);
 };
