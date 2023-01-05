62797b62b30f8e274f9556b5d426245f18b3890e
Olivier mathoul <Barney@live.fr>
2023-01-05 23:22:49 +0100

    feat: :fire: Add token for test

--- api/test/Users/createUser.test.js
+++ api/test/Users/createUser.test.js
@@ -59,44 +59,6 @@ module.exports = (libs, options) => {
       assert.equal(response.status, 500);
     });
 
-    
-
-    it('should return status 500 if color is not filled', async () => {
-      const response = await fetch(`${baseUrl}/user/register`, {
-        headers,
-        method: 'POST',
-        body: JSON.stringify({
-          first_name: faker.name.firstName(),
-          last_name: faker.name.lastName(),
-          email: faker.internet.email().toLowerCase(),
-          password: faker.internet.password(),
-        }),
-      }).then((res) => ({
-        body: res.json(),
-        status: res.status,
-      }));
-
-      assert.equal(response.status, 500);
-    });
-
-    it('should return status 500 if email is not filled', async () => {
-      const response = await fetch(`${baseUrl}/user/register`, {
-        headers,
-        method: 'POST',
-        body: JSON.stringify({
-          first_name: faker.name.firstName(),
-          last_name: faker.name.lastName(),
-          email: "",
-          password: faker.internet.password(),
-          color: faker.internet.color(),
-        }),
-      }).then((res) => ({
-        body: res.json(),
-        status: res.status,
-      }));
-
-      assert.equal(response.status, 500);
-    });
 
     it('should return status 500 if password is not filled', async () => {
       const response = await fetch(`${baseUrl}/user/register`, {
