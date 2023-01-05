c3fbd730430f7c008f90fe12e8f30c6a5c447710
WildTrack <wildryx@outlook.fr>
2023-01-05 08:36:44 +0100

    feat: :sparkles: Add constants for all tests

--- api/test/index.js
+++ api/test/index.js
@@ -1,4 +1,22 @@
+const assert = require('assert');
+const fetch = require('node-fetch');
+const { faker } = require('@faker-js/faker');
 
+const baseUrl = 'http://localhost:3001';
+const headers = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+};
 
-const testList = require('./Lists');
-testList();
+const libs = {
+  assert,
+  fetch,
+  faker,
+};
+
+const options = {
+  baseUrl,
+  headers,
+};
+
+require('./Users')(libs, options);
