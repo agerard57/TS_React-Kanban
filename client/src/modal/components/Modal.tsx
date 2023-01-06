f185a14020fcf5d0a6e3d552fdbc6b6eda147a0f
GERARD Alexandre <agerard57@protonmail.com>
2023-01-06 01:17:47 +0100

    feat: Fix modal

--- client/src/modal/components/Modal.tsx
+++ client/src/modal/components/Modal.tsx
@@ -14,36 +14,37 @@ export const Modal: FC = () => {
     <div id="modal" css={modalStyles(isOpen)} className={isOpen ? 'opened' : 'closed'}>
       <Container
         css={css`
-          height: 100%;
-          width: 100%;
+          height: 50%;
+          width: 50%;
           padding: 2rem;
           display: flex;
-          flex-direction: column;
           justify-content: space-between;
           font-family: 'Baloo2';
           text-align: center;
+          background: #fffffff1;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          border-radius: 10px;
         `}
       >
-        <Row
+        <div
           css={css`
             flex: auto;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
             overflow-y: auto;
             border-radius: 10px;
             border: 1px solid #e0e0e0;
             padding: 1rem;
+            text-align: center;
           `}
         >
           {slideSpec.content}
-        </Row>
-        <Row
-          css={css`
-            position: inherit;
-            padding: 1rem 0.2rem 0 0;
-            background: #ffffff;
-          `}
-        >
           <Button closeButtonText={slideSpec.closeButtonText} close={slideSpec.closeButtonAction} />
-        </Row>
+        </div>
       </Container>
     </div>
   );
