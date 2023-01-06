99d213f49bf505badf2c432a87f9fd260053c90f
GERARD Alexandre <agerard57@protonmail.com>
2023-01-06 03:30:28 +0100

    feat: Remove index

--- client/src/core/components/index.ts
+++ client/src/core/components/index.ts
@@ -1,6 +1,5 @@
 export { Layout } from './Layout';
 export { Mobile } from './Mobile';
 export { KanbanList } from './kanban';
-export { Navbar } from './Navbar';
 export { Inputs } from './inputs';
 export { Button } from './Button';
