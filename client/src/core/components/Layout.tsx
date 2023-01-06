a9910b52ca7f6c3d1819e9ddff66780567e0861c
GERARD Alexandre <agerard57@protonmail.com>
2023-01-06 01:18:21 +0100

    feat: Various changes

--- client/src/core/components/Layout.tsx
+++ client/src/core/components/Layout.tsx
@@ -1,11 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import { FC } from 'react';
 
+import { Modal } from '../../modal';
 import { ColoredBackground } from './ColoredBackground';
 import { Navbar } from './Navbar';
 
 export const Layout: FC = ({ children }) => (
   <>
+    <Modal />
     <Navbar />
     <div className="App">{children}</div>
     <ColoredBackground />
