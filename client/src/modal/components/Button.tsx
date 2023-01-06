0ea772ed1ca56749ddac82a62605f52f5cbd88fd
GERARD Alexandre <agerard57@protonmail.com>
2023-01-06 01:17:47 +0100

    feat: Fix modal

--- client/src/modal/components/Button.tsx
+++ client/src/modal/components/Button.tsx
@@ -3,25 +3,30 @@ import { css } from '@emotion/react';
 import { FC } from 'react';
 
 import { Inputs } from '../../core';
+import { useModal } from '../hooks';
 
 type Props = {
   closeButtonText?: string;
   close?: () => void;
 };
 
-export const Button: FC<Props> = ({ closeButtonText, close }) => (
-  <>
-    {closeButtonText && (
-      <Inputs.Button
-        type="small"
-        onClick={close}
-        optionalStyling={css`
-          padding-top: 0.5rem;
-          background: transparent;
-        `}
-      >
-        {closeButtonText}
-      </Inputs.Button>
-    )}
-  </>
-);
+export const Button: FC<Props> = ({ closeButtonText, close }) => {
+  const { closeModal } = useModal();
+
+  return (
+    <>
+      {closeButtonText && (
+        <Inputs.Button
+          type="small"
+          onClick={close || closeModal}
+          optionalStyling={css`
+            padding-top: 0.5rem;
+            background: transparent;
+          `}
+        >
+          {closeButtonText}
+        </Inputs.Button>
+      )}
+    </>
+  );
+};
