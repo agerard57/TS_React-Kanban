d1b984c379bb3ca7397362a3fc08ff8e9561a584
WildTrack <wildryx@outlook.fr>
2023-01-06 08:48:06 +0100

    feat: :sparkles: Added connection for the creation list test

--- api/test/Lists/createList.test.js
+++ api/test/Lists/createList.test.js
@@ -1,26 +1,18 @@
-// const assert = require('assert');
-// const fetch = require('node-fetch');
-
 module.exports = (libs, options) => {
   const { assert, fetch } = libs;
   const { headers, baseUrl } = options;
   describe('Test for creation of list', () => {
-    it('should return status 200', async () => {;
+    it('should return status 200', async () => {
       const response = await fetch(`${baseUrl}/list`, {
-        
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${options.token}`
-        },
+        headers,
         method: 'POST',
-        body: {
+        body: JSON.stringify({
           title: 'Tset',
-          user_id: 0
-        }
+          user_id: 0,
+        }),
       }).then((res) => ({
-          'body': res.json(),
-          'status': res.status
+        body: res.json(),
+        status: res.status,
       }));
 
       assert.equal(response.status, 200);
--- api/test/Lists/index.js
+++ api/test/Lists/index.js
@@ -1,16 +1,12 @@
-/* eslint-disable no-param-reassign */
-
 module.exports = (libs, options) => {
-
-  const headers = {...options.headers, Authorization: `Bearer ${options.token}`};
+  const headers = {
+    ...options.headers,
+    Authorization: `Bearer ${options.token}`,
+  };
   const optionsWithToken = {
     headers,
-    baseUrl: options.baseUrl
+    baseUrl: options.baseUrl,
   };
-
-  console.log(optionsWithToken);
 
   require('./createList.test')(libs, optionsWithToken);
 };
-
-
